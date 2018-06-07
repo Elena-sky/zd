905ad1f005f6334e7ce98221e4f4f4274420611a
kry-dev <yaroslavkryvda@gmail.com>
2018-06-07 17:13:23 +0300

    animation welcome messages

--- build/assets/js/multi-step-modal.js
+++ build/assets/js/multi-step-modal.js
@@ -7,53 +7,11 @@
         var $modal = $(modal);
         var $bodies = $modal.find('div.modal-body');
         var total_num_steps = $bodies.length;
-        var $progress = $modal.find('.m-progress');
-        var $progress_bar = $modal.find('.m-progress-bar');
-        var $progress_stats = $modal.find('.m-progress-stats');
-        var $progress_current = $modal.find('.m-progress-current');
-        var $progress_total = $modal.find('.m-progress-total');
-        var $progress_complete  = $modal.find('.m-progress-complete');
         var reset_on_close = $modal.attr('reset-on-close') === 'true';
 
         function reset() {
-            $modal.find('.step').hide();
-            $modal.find('[data-step]').hide();
-        }
-
-        function completeSteps() {
-            $progress_stats.hide();
-            $progress_complete.show();
-            $modal.find('.progress-text').animate({
-                top: '-2em'
-            });
-            $modal.find('.complete-indicator').animate({
-                top: '-2em'
-            });
-            $progress_bar.addClass('completed');
-        }
-
-        function getPercentComplete(current_step, total_steps) {
-            return Math.min(current_step / total_steps * 100, 100) + '%';
-        }
-
-        function updateProgress(current, total) {
-            $progress_bar.animate({
-                width: getPercentComplete(current, total)
-            });
-            if (current - 1 >= total_num_steps) {
-                completeSteps();
-            } else {
-                $progress_current.text(current);
-            }
-
-            $progress.find('[data-progress]').each(function() {
-                var dp = $(this);
-                if (dp.data().progress <= current - 1) {
-                    dp.addClass('completed');
-                } else {
-                    dp.removeClass('completed');
-                }
-            });
+            $modal.find('.step').removeClass('d-block animated');
+            $modal.find('[data-step]').removeClass('d-block animated');
         }
 
         function goToStep(step) {
@@ -63,9 +21,11 @@
                 // at the last step, nothing else to show
                 return;
             }
-            to_show.show();
-            var current = parseInt(step, 10);
-            updateProgress(current, total_num_steps);
+            var animateWelcomeText = function () {
+                $('.welcome-modal .animated').removeClass('animated');
+                $(this).addClass('animated');
+            };
+            to_show.toggleClass('d-block animated');
             findFirstFocusableInput(to_show).focus();
         }
 
@@ -100,24 +60,17 @@
 
         function initialize() {
             reset();
-            updateProgress(1, total_num_steps);
-            $modal.find('.step-1').show();
-            $progress_complete.hide();
-            $progress_total.text(total_num_steps);
+            $modal.find('.step-1').addClass('d-block animated');
             bindEventsToModal($modal, total_num_steps);
             $modal.data({
                 total_num_steps: $bodies.length,
             });
             if (reset_on_close){
-                //Bootstrap 2.3.2
-                $modal.on('hidden', function () {
-                    reset();
-                    $modal.find('.step-1').show();
-                });
                 //Bootstrap 3
                 $modal.on('hidden.bs.modal', function () {
                     reset();
-                    $modal.find('.step-1').show();
+                    //$modal.find('.step-1').show();
+                    $modal.find('.step-1').addClass('d-block');
                 })
             }
         }
