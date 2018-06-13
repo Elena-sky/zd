a17689835ed94adb0a0b254e3c6841e987927264
kry-dev <yaroslavkryvda@gmail.com>
2018-06-13 23:49:58 +0300

    add 2nd page

--- src/js/app.js
+++ src/js/app.js
@@ -2,45 +2,7 @@ var $ = require('jquery');
 var slick = require('slick-carousel');
 
 $(document).ready(function(){
-    $('.new-slider').slick({
-        dots: true,
-        nextArrow: '<button class="right"><i class="fa fa-arrow-right"></i></button>',
-        prevArrow: '<button class="left"><i class="fa fa-arrow-left"></i></button>'
+    $('.star.rating').click(function(){
+        $(this).parent().attr('data-stars', $(this).data('rating'));
     });
-    $('.brands-slider').slick({
-        arrows: true,
-        slidesToShow: 6,
-        slidesToScroll: 1,
-        rows: 1,
-        nextArrow: '<button class="right"><i class="fa fa-arrow-right"></i></button>',
-        prevArrow: '<button class="left"><i class="fa fa-arrow-left"></i></button>',
-        responsive: [
-            {
-                breakpoint: 700,
-                settings: {
-                    slidesToShow: 3,
-                    rows:2
-                }
-            },
-            {
-                breakpoint: 500,
-                settings: {
-                    slidesToShow: 2,
-                    rows:1
-                }
-            }
-        ]
-    });
-
-    var hideTooltip = function () {$('#calendar .calendar-tooltip').removeClass('d-block');};
-    $('.btn-tooltip-close').on('click', function () {
-        $('.calendar-tooltip').removeClass('d-block');
-    });
-
-    $('.events-link').on('click', function() {
-        hideTooltip();
-        $(this).next('.calendar-tooltip').addClass('d-block');
-
-    });
-
 });
