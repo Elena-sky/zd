471816a3a1c02ea110e2b0b68df91bd4c19b2051
kry-dev <yaroslavkryvda@gmail.com>
2018-06-21 13:36:47 +0300

    add 6 page - conclution

--- src/js/app.js
+++ src/js/app.js
@@ -8,4 +8,16 @@ $(document).ready(function(){
     $('.small-trees-item.rating').click(function(){
         $(this).parent().attr('data-stars', $(this).data('rating'));
     });
+    var cols =  $("" +
+        " .row.oneHeight > .col-xs-6," +
+        " .row.oneHeight > .col-sm-6," +
+        " .row.oneHeight > .col-md-6," +
+        " .row.oneHeight > .col-lg-6");
+    var heights = cols.map(function() {
+            return $(this).height();
+        }).get(),
+
+        maxHeight = Math.max.apply(null, heights);
+
+    $(cols).height(maxHeight);
 });
